0595e87cfe293b4ef48b05d79be4c7ed77b6e1dc
ibrahim-27 <105308089+ibrahim-27@users.noreply.github.com>
2022-07-26 22:32:00 +0500

    Receiver's home page food list shows only the food donated in the same city.

--- app/src/main/java/com/example/fooddonation/ui/home/ReceiverHomeFragment.kt
+++ app/src/main/java/com/example/fooddonation/ui/home/ReceiverHomeFragment.kt
@@ -4,15 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.fooddonation.Adapter_Dataclasses.DonorHistoryAdapter
 import com.example.fooddonation.Adapter_Dataclasses.ReceiverFoodListAdapter
-import com.example.fooddonation.Adapter_Dataclasses.donor_history_data
 import com.example.fooddonation.Adapter_Dataclasses.receiver_food_list
 import com.example.fooddonation.databinding.FragmentReceiverHomeBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class ReceiverHomeFragment : Fragment() {
 
@@ -28,14 +32,56 @@ class ReceiverHomeFragment : Fragment() {
 		_binding = FragmentReceiverHomeBinding.inflate(inflater, container, false)
 		val root: View = binding.root
 
+		/** Food List and adapter for recycler view **/
 		val foodList: ArrayList<receiver_food_list> = ArrayList()
 		val adapter = ReceiverFoodListAdapter(foodList,this.requireContext())
-		foodList.add(receiver_food_list("Pizza", "Home-Made"))
-		foodList.add(receiver_food_list("Kheer", "Desi"))
-		foodList.add(receiver_food_list("Burger", "Fast food"))
-		foodList.add(receiver_food_list("Pizza", "Home-Made"))
-		foodList.add(receiver_food_list("Karahi", "Desi"))
-		foodList.add(receiver_food_list("Burger", "Fast food"))
+
+		// initializing auth
+		val auth = Firebase.auth
+
+		/** Database call **/
+		val database = Firebase.database
+		val ref = auth.currentUser?.let { database.getReference("Users").child("Receiver").child(it.uid) }
+
+		var receiverCity:String = ""
+
+		// Getting the city of the current user - receiver
+		ref?.addValueEventListener(object: ValueEventListener{
+			override fun onDataChange(snapshot: DataSnapshot) {
+				receiverCity = snapshot.child("city").value.toString()
+			}
+			override fun onCancelled(error: DatabaseError) {
+				Toast.makeText(context, error.toString(), Toast.LENGTH_SHORT).show()
+			}
+		})
+
+		// All food donated in the same city
+		val ref2 = database.getReference("Food")
+		ref2.addValueEventListener(object: ValueEventListener{
+			override fun onDataChange(snapshot: DataSnapshot) {
+				for (food in snapshot.children)
+				{
+					// Getting donor's city
+					val tempRef = database.getReference("Users").child("Donor").child(food.child("donor_id").value.toString())
+					tempRef.addValueEventListener(object: ValueEventListener{
+						override fun onDataChange(snapshot: DataSnapshot) {
+							// checking if the donor and receiver are in the same city
+							if(snapshot.child("city").value.toString() == receiverCity)
+							{
+								foodList.add(receiver_food_list(food.child("name").value.toString(), food.child("type").value.toString()))
+								binding.rcvFoodList.adapter = adapter
+							}
+						}
+						override fun onCancelled(error: DatabaseError) {
+							Toast.makeText(context, error.toString(), Toast.LENGTH_SHORT).show()
+						}
+					})
+				}
+			}
+			override fun onCancelled(error: DatabaseError) {
+				Toast.makeText(context, error.toString(), Toast.LENGTH_SHORT).show()
+			}
+		})
 
 		binding.rcvFoodList.adapter = adapter
 		binding.rcvFoodList.layoutManager = LinearLayoutManager(this.requireContext())
